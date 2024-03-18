7e7ac1bb0efe043e5229e5f90f1fcfdd85f44954
MYNAMEYOGESH <2001yogesh2001yogesh@gmail.com>
2024-03-18 14:20:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Menu from './Component/default/Menu'
+import Container from './Pages/Container'
+import { ToastContainer } from 'react-toastify'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <ToastContainer autoClose ={4000} position="top-right" />
+      <Container />
+    </BrowserRouter>
+  )
+}
+
+export default App
