c22f3ffcfa64fd4a303f1786401d01608af85a10
MYNAMEYOGESH <2001yogesh2001yogesh@gmail.com>
2024-03-19 14:12:57 +0530

    19/3/24

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import Menu from './Component/default/Menu'
 import Container from './Pages/Container'
 import { ToastContainer } from 'react-toastify'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter,  } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
       <Menu />
       <ToastContainer autoClose ={4000} position="top-right" />
+      <div className="container-main"> 
       <Container />
+      </div>
+      
+     
     </BrowserRouter>
   )
 }
